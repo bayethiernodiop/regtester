6ffa4069ec402fdb6d75a06691483da28f1f5c03
Thierno Diop <thierno.diop@baamtu.com>
2019-11-12 14:06:36 +0000

    deploy to pypi with command line possibilities

--- regtester/regtester/__init__.py
+++
@@ -1,5 +0,0 @@
-from regtester.RegExTestItem import RegExTestItem
-from regtester.RegExTester import RegExTester
-from regtester.RegExTest import RegExTest
-from regtester.TestItemResult import TestItemResult
-from regtester.TestResponse import TestResponse
